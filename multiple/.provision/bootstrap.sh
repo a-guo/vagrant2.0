0afc7fad098a9b676d4b806b03e22df3bf9b0c92
Addie Guo <addieguo98@gmail.com>
2019-07-02 09:04:28 -0700

    work in progress

--- multiple/.provision/bootstrap.sh
+++ multiple/.provision/bootstrap.sh
@@ -7,7 +7,7 @@ sudo service nginx start
 # set up nginx server
 sudo cp /vagrant/.provision/nginx/nginx.conf /etc/nginx/sites-available/site.conf
 sudo chmod 644 /etc/nginx/sites-available/site.conf
-sudo ln -s /etc/nginx/sites-available/site-multi.conf /etc/nginx/sites-enabled/site-multi.conf
+sudo ln -s /etc/nginx/sites-available/site-multi.conf /etc/nginx/sites-enabled/site.conf
 sudo service nginx restart
 
 # clean /var/www
